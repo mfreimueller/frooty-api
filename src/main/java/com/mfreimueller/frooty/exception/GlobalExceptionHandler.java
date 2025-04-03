2221bc05294c5a7d2f21826c44f716df9400dc60
Michael Freimüller <michael@mfreimueller.com>
2025-04-03 13:56:52 +0200

    log unexpected exceptions in GlobalExceptionHandler

--- src/main/java/com/mfreimueller/frooty/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mfreimueller/frooty/exception/GlobalExceptionHandler.java
@@ -1,12 +1,17 @@
 package com.mfreimueller.frooty.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleEntityNotFound(EntityNotFoundException ex) {
@@ -22,6 +27,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleGenericException(Exception ex) {
+        logger.error("An unknown error occurred.", ex);
+
         return "An internal error occurred.";
     }
 }
