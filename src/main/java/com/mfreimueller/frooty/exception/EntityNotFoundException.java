40892972d4f66391ff0498b4b19d2613854489a4
Michael Freimüller <michael@mfreimueller.com>
2025-04-03 13:56:03 +0200

    extend EntityNotFoundException to display type of entity

--- src/main/java/com/mfreimueller/frooty/exception/EntityNotFoundException.java
+++ src/main/java/com/mfreimueller/frooty/exception/EntityNotFoundException.java
@@ -3,7 +3,11 @@ package com.mfreimueller.frooty.exception;
 public class EntityNotFoundException extends RuntimeException {
 
     public EntityNotFoundException(Integer id) {
-        super("Could not find meal " + id);
+        super("Could not find entity " + id);
+    }
+
+    public EntityNotFoundException(Integer id, String entity) {
+        super("Could not find " + entity + " " + id);
     }
 
 }
