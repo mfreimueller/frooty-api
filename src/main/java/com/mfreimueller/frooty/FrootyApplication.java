50e200a2dbc4d6348645fbe6bc5e5403b5847809
Michael Freimüller <michael@mfreimueller.com>
2025-04-29 12:15:25 +0200

    extend Application class to support war files

--- src/main/java/com/mfreimueller/frooty/FrootyApplication.java
+++ src/main/java/com/mfreimueller/frooty/FrootyApplication.java
@@ -2,9 +2,16 @@ package com.mfreimueller.frooty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FrootyApplication {
+public class FrootyApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(FrootyApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FrootyApplication.class, args);
